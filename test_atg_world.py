571b76ef5c652ac4f74a12548a7c9dbb6dae084e
POOJITHA GOLUSULA <97933611+poojitha1783@users.noreply.github.com>
2023-12-19 12:23:24 +0530

    Update test_atg_world.py

--- test_atg_world.py
+++ test_atg_world.py
@@ -1,11 +1,24 @@
-# test_atg_world.py
-import requests
 import unittest
+import requests
 
-class TestAtgWorldWebsite(unittest.TestCase):
-    def test_website_access(self):
-        response = requests.get("https://atg.world")
-        self.assertEqual(response.status_code, 200, "Website did not load successfully")
+class TestWebsiteAvailability(unittest.TestCase):
+    def test_website_loading(self):
+        url = "https://www.atg.world"  # Replace with the correct your Website  URL
 
-if __name__ == "__main__":
-    unittest.main()
+        print(f"Attempting to connect to {url}...")
+
+        try:
+            response = requests.get(url)
+            status_code = response.status_code
+
+            print(f"Status code received: {status_code}")
+
+            # Check if the status code is in the 2xx range (indicating success)
+            self.assertTrue(status_code >= 200 and status_code < 300)
+            print("Website loaded successfully!")
+        except requests.RequestException as e:
+            print(f"Failed to connect to the website: {e}")
+            self.fail("Website loading failed")
+
+if _name_ == '_main_':
+    unittest.main()
